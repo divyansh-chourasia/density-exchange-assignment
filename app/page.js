a5175628cf7bb8686de198d0e9d74b2c0ac16198
Divyansh Chourasia <110674394+divyansh-chourasia@users.noreply.github.com>
2023-10-26 13:40:08 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,18 @@
-import Image from 'next/image'
+import Footer from "@/components/Footer";
+import Awareness from "@/pages/Awareness";
+import Emotions from "@/pages/Emotions";
+import Manifesto from "@/pages/Manifesto";
+import Work from "@/pages/Work";
 
 export default function Home() {
   return (
-    <main className="">
-     Hello
+    <main className="flex-grow overflow-y-auto mt-20">
+      <Emotions />
+      <Manifesto />
+      <Awareness />
+      <Work />
+
+      <Footer />
     </main>
-  )
+  );
 }
